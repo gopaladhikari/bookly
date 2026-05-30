e41d67d31292d63bb593c575d030cdf02be6e786
gopaladhikari <gopuadks@gmail.com>
2026-05-30 15:33:22 +0545

    book model

---
+++ src/books/models.py
@@ -0,0 +1,41 @@
+from sqlmodel import Field, SQLModel, Column, func
+import sqlalchemy.dialects.postgresql as pg
+from datetime import datetime, timezone
+from uuid import UUID, uuid4
+
+
+def now():
+    return datetime.now(timezone.utc)
+
+
+class Book(SQLModel, table=True):
+    __tablename__ = "books"  # type: ignore
+
+    id: UUID = Field(
+        sa_column=Column(
+            pg.UUID(as_uuid=True), nullable=False, primary_key=True, default=uuid4
+        )
+    )
+    title: str
+    author: str
+    description: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+
+    created_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP(timezone=True), default=now, nullable=False)
+    )
+
+    updated_at: datetime = Field(
+        sa_column=Column(
+            pg.TIMESTAMP(timezone=True),
+            default=now,
+            onupdate=func.now(),
+            nullable=False,
+        )
+    )
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
