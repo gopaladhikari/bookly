9d0902c0278cf3ed204b252a767b1ac94de41ac3
gopaladhikari <gopuadks@gmail.com>
2026-06-04 09:28:05 +0545

    added user model

--- src/auth/model.py
+++ src/auth/model.py
@@ -1,33 +1,31 @@
-from sqlmodel import Field, SQLModel, Column, func
-import sqlalchemy.dialects.postgresql as pg
-from datetime import datetime, timezone
+from sqlmodel import SQLModel, Column, Field, func
 from uuid import uuid4
+from datetime import datetime, timezone
+import sqlalchemy.dialects.postgresql as pg
 
 
 def now():
     return datetime.now(timezone.utc)
 
 
-class Book(SQLModel, table=True):
-    __tablename__ = "books"  # type: ignore
+class User(SQLModel, table=True):
+    __tablename__ = "users"  # type: ignore
 
     id: pg.UUID = Field(
         sa_column=Column(
-            pg.UUID(as_uuid=True), nullable=False, primary_key=True, default=uuid4
+            pg.UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid4
         )
     )
-    title: str
-    author: str
-    description: str
-    publisher: str
-    published_date: datetime
-    page_count: int
-    language: str
 
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    is_verified: bool = False
     created_at: datetime = Field(
         sa_column=Column(pg.TIMESTAMP(timezone=True), default=now, nullable=False)
     )
-
     updated_at: datetime = Field(
         sa_column=Column(
             pg.TIMESTAMP(timezone=True),
@@ -38,4 +36,4 @@ class Book(SQLModel, table=True):
     )
 
     def __repr__(self):
-        return f"<Book {self.title}>"
+        return f"<User {self.username}>"
